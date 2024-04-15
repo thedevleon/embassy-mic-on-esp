339dc702a2ca263d9269b34162a65847fff7a9d7
Leon <12817042+thedevleon@users.noreply.github.com>
2024-04-15 11:23:08 +0200

    feat: add burn (and alloc)

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,23 @@ use esp_hal::{
 use esp_println::println;
 // use embassy_time::Instant;
 
+extern crate alloc;
+use core::mem::MaybeUninit;
+#[global_allocator]
+static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
+
+fn init_heap() {
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
+
+    unsafe {
+        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
+    }
+}
+
 #[main]
 async fn main(_spawner: Spawner) {
-    println!("Init!");
+    init_heap();
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
